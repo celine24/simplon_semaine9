68c54edf0919652973a76071a58b07c695afc0a4
celine24 <peyrot.celine@gmail.com>
2016-02-06 15:40:35 +0000

    ajout exercice objets

--- annuaire/js/annuaire.js
+++ annuaire/js/annuaire.js
@@ -1,27 +1,54 @@
+$list = document.getElementById("list");
+$firstname = document.getElementById("firstname");
+$lastname = document.getElementById("lastname");
+$phoneNumber = document.getElementById("phoneNumber");
+$add = document.getElementById("add");
+$searchResult = document.getElementById("searchResult");
+
 var contacts = [{
-  prenom: "Julien",
-  nom: "Grillot",
-  numero: "0123456789"
+    firstname: "Julien",
+    lastname: "Grillot",
+    phoneNumber: "0123456789"
 }, {
-  prenom: "John",
-  nom: "Smith",
-  numero: "0123456789"
+    firstname: "John",
+    lastname: "Smith",
+    phoneNumber: "0123456789"
 }, {
-    prenom: "John",
-    nom: "Doe",
-    numero: "0196874523"
+    firstname: "John",
+    lastname: "Doe",
+    phoneNumber: "0196874523"
 }];
 
-function texteContact(contact) {
-    for (var i = 0, c = contacts.length; i < c ; i++) {
-        if (contact === contacts[i]) {
-            return contacts[i].prenom + " " + contacts[i].nom + " (" + contacts[i].numero + ")";
-        }
-        else {
-            return "Désolé, ce contact n'existe pas.";
-        }
-    }
+function textContact(contact) {
+    return contact.firstname + " " + contact.lastname + " (" + contact.phoneNumber + ")";
 }
 
-//texteContact(); 
-alert(texteContact(contacts[0])); // affiche « Julien Grillot (0123456789) »
+function displayList() {
+    elements = "";
+    for (i = 0, c = contacts.length; i < c; i++) {
+        elements += "<li>" + textContact(contacts[i]) + "</li>";
+    }
+    $list.innerHTML = elements;
+}
+
+function add() {
+    contacts.push({
+        firstname: $firstname.value,
+        lastname: $lastname.value,
+        phoneNumber: $phoneNumber.value
+    });
+    displayList();
+}
+
+function search(contact) {
+    contacts.push({
+        firstname: $firstname.value,
+        lastname: $lastname.value,
+        phoneNumber: $phoneNumber.value
+    });
+    displayList();
+}
+
+
+$add.onclick = add;
+displayList();
